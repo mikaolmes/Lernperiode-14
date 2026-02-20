e14ce23dc8efe714228c2a7a1bf39e2fbfcdd792
Mika Olmes <M.Olmes.inf23@stud.bbbaden.ch>
2026-02-20 16:25:10 +0100

    Hello World Code

---
+++ main.py
@@ -0,0 +1,28 @@
+import customtkinter
+
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("dark-blue")
+
+root = customtkinter.CTk()
+root.geometry("500x300")
+
+def login():
+    print("Hello World")
+
+
+frame = customtkinter.CTkFrame(master=root)
+frame.pack(pady=20, padx=60, fill="both", expand=True)
+
+label = customtkinter.CTkLabel(master=frame, text="Hello World Test Program", font=("Roboto", 24))
+label.pack(pady=12, padx=10)
+
+entry1 = customtkinter.CTkEntry(master=frame, placeholder_text="Type something here...")
+entry1.pack(pady=12, padx=10)
+
+button = customtkinter.CTkButton(master=frame, text="Press to Print Hello World", command=login)
+button.pack(pady=12, padx=10)
+
+checkbox = customtkinter.CTkCheckBox(master=frame, text="Try me!")
+checkbox.pack(pady=12, padx=10)
+
+root.mainloop()
