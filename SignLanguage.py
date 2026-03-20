c5c8d113b357edb763c80a0d8c898576d47c9041
Mika Olmes <M.Olmes.inf23@stud.bbbaden.ch>
2026-03-20 14:48:33 +0100

    new Hotkeys for meme_movement

--- SignLanguage.py
+++ SignLanguage.py
@@ -15,9 +15,12 @@ HandLandmarkerOptions = mp.tasks.vision.HandLandmarkerOptions
 VisionRunningMode = mp.tasks.vision.RunningMode
 
 HAND_CONNECTIONS = [
-    (0, 1), (1, 2), (2, 3), (3, 4), (0, 5), (5, 6), (6, 7), (7, 8),
-    (0, 9), (9, 10), (10, 11), (11, 12), (0, 13), (13, 14), (14, 15), (15, 16),
-    (0, 17), (17, 18), (18, 19), (19, 20), (5, 9), (9, 13), (13, 17)
+    (0, 1), (1, 2), (2, 3), (3, 4),
+    (0, 5), (5, 6), (6, 7), (7, 8),
+    (0, 9), (9, 10), (10, 11), (11, 12),
+    (0, 13), (13, 14), (14, 15), (15, 16),
+    (0, 17), (17, 18), (18, 19), (19, 20),
+    (5, 9), (9, 13), (13, 17)
 ]
 
 MODEL_PATH = os.path.join(os.path.dirname(__file__), "hand_landmarker.task")
